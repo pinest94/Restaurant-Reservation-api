27bb70a8231fb21c4fff6c2697590587fe97eed9
mentalK94 <doingnow94@gmail.com>
2020-02-05 22:00:55 +0900

    메뉴 삭제기능 추가

--- restaurantReservation-api/src/test/java/kr/co/mentalK94/restaurantReservation/application/MenuItemServiceTest.java
+++ restaurantReservation-api/src/test/java/kr/co/mentalK94/restaurantReservation/application/MenuItemServiceTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -35,6 +36,7 @@ public class MenuItemServiceTest {
                 .name("Kimchi")
                 .build());
         menuItems.add(MenuItem.builder()
+                .id(12L)
                 .name("Gukbap")
                 .build());
         menuItems.add(MenuItem.builder()
@@ -43,9 +45,11 @@ public class MenuItemServiceTest {
         menuItems.add(MenuItem.builder()
                 .name("Bibimbap")
                 .build());
+        menuItems.add(MenuItem.builder().id(1004L).destroy(true).build());
         menuItemService.bulkUpdate(1L, menuItems);
 
         verify(menuItemRepository, times(4)).save(any());
+        verify(menuItemRepository, times(1)).deleteById(eq(1004L));
     }
 
 }
